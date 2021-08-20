76c3a50cafebb9e7bcb33fa509284d43132ee63f
Jeffrey Smith <i.am.hef@gmail.com>
2021-08-20 15:22:55 -0700

    Update 401kcalc3.py
    add formatting

--- 401kcalc3.py
+++ 401kcalc3.py
@@ -3,7 +3,7 @@
 #variables_in_snake_case#
 salary = int(input("Enter current annual base salary:"))
 ytd_contributions = int(input("Enter your contributions to date:"))
-remaining_pay_periods = int(input("How many pay periods are left in the year?"))
+remaining_pay_periods = int(input("How many semi-monthly pay periods are left in the year?"))
 age = int(input("Enter your age this year:"))
 
 CURRENT_YEAR_MAXIMUM_CONTRIBUTION_UNDER_50 = 19500.0
@@ -15,8 +15,8 @@ if age < 50:
 if age >= 50:
     remaining_contributions = (CURRENT_YEAR_MAXIMUM_CONTRIBUTION_OVER_50 - ytd_contributions)
 
-new_pay_period_dollar_amount = float(remaining_contributions / remaining_pay_periods)
-new_pay_period_percent_amount = float(remaining_contributions / remaining_pay_periods) / (salary / 24)
+new_pay_period_dollar_amount = "${:,.2f}".format(remaining_contributions / remaining_pay_periods)
+new_pay_period_percent_amount = "{:,.2f}".format(int(remaining_contributions / remaining_pay_periods) / (salary / 24)) + "%"
 # calculating the output#
 
 print("The amount per paycheck will be: ")
